eb3dc849457b8b700c52ccc0eb9cc80262631d06
João Anderson <joao.anderson.af@gmail.com>
2025-06-15 11:51:59 -0300

    Verificando quantidade de folgas dos militares

--- apis/src/main/java/apisemaperreio/escalante/Instanciation.java
+++ apis/src/main/java/apisemaperreio/escalante/Instanciation.java
@@ -1,7 +1,10 @@
 package apisemaperreio.escalante;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Comparator;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -22,7 +25,7 @@ public class Instanciation implements CommandLineRunner {
 
         var dataInicio = LocalDate.parse("2024-09-01");
         var dataFim = LocalDate.parse("2024-09-30");
-        
+
         var militares = militarUseCasesEscalante.listarMilitaresEscalaveis(dataInicio, dataFim);
 
         var escala = new Escala(dataInicio, dataFim, 2);
@@ -32,6 +35,28 @@ public class Instanciation implements CommandLineRunner {
         escala.getMilitaresEscalados().sort(Comparator.comparing(ServicoOperacional::getDataServico));
 
         escala.getMilitaresEscalados().forEach(System.out::println);
+
+        for (var militar : militares) {
+            var dataServicos = escala.getMilitaresEscalados().stream()
+                    .filter(s -> {
+                        var matriculaMilitar = Optional.ofNullable(s.getMatriculaMilitar());
+                        if (matriculaMilitar.isEmpty())
+                            return false;
+                        return s.getMatriculaMilitar().equals(militar.getMatricula());
+                    })
+                    .map(ServicoOperacional::getDataServico).collect(Collectors.toList());
+            System.out.println(militar);
+            System.out.println("Data dos serviços: " + dataServicos);
+            var folgas = new ArrayList<Integer>();
+            for (var i = 1; i < dataServicos.size() - 1; i += 2) {
+                if (dataServicos.size() > 2) {
+                    var folga = dataServicos.get(i + 1).compareTo(dataServicos.get(i));
+                    folgas.add(folga);
+                }
+            }
+
+            System.out.println("Folgas: " + folgas);
+        }
 
         // Arrays.stream(Funcao.values()).forEach(System.out::println);
 
