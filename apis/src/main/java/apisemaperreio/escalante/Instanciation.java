005c29343cc5b8078cd3162399d1ec5318415de4
João Anderson <joao.anderson.af@gmail.com>
2025-07-13 18:37:28 -0300

    Testando importação da planilha

--- apis/src/main/java/apisemaperreio/escalante/Instanciation.java
+++ apis/src/main/java/apisemaperreio/escalante/Instanciation.java
@@ -1,48 +1,38 @@
 package apisemaperreio.escalante;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.web.multipart.MultipartFile;
+
+import apisemaperreio.escalante.escalante.utils.adapters.ImportadorMilitaresXLSXAdapter;
 
 @Configuration
 public class Instanciation implements CommandLineRunner {
 
-    // @Autowired
-    // private EscalaUseCasesEscalante escalaUseCases;
+    @Autowired
+    private ImportadorMilitaresXLSXAdapter importadorMilitaresXLSXAdapter;
 
     @Override
     public void run(String... args) throws Exception {
 
-        // var dataInicio = LocalDate.parse("2024-09-01");
-        // var dataFim = LocalDate.parse("2024-09-30");
+        File file = new File("../private/Planilha Importação Militares.xlsx");
 
-        // var dadosEscala = new DadosEscala(dataInicio, dataFim, 2);
+        InputStream inputStream = new FileInputStream(file);
 
-        // var escala = escalaUseCases.criarEscalaAutomatica(dadosEscala);
+        MultipartFile multipartFile = new MockMultipartFile(
+                "file", file.getName(), 
+                "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+                inputStream);
 
-        // escala.forEach(System.out::println);
+        var militares = importadorMilitaresXLSXAdapter.importarMilitaresXLSX(multipartFile);
 
-        // for (var militar : militares) {
-        //     var dataServicos = escala.getMilitaresEscalados().stream()
-        //             .filter(s -> {
-        //                 var matriculaMilitar = Optional.ofNullable(s.getMilitar().getMatricula());
-        //                 if (matriculaMilitar.isEmpty())
-        //                     return false;
-        //                 return s.getMilitar().getMatricula().equals(militar.getMatricula());
-        //             })
-        //             .map(ServicoOperacional::getDataServico)
-        //             .distinct().collect(Collectors.toList());
-        //     System.out.println(militar);
-        //     System.out.println("Data dos serviços: " + dataServicos);
-        //     var folgas = new ArrayList<Integer>();
-        //     for (var i = 1; i < dataServicos.size() - 1; i += 2) {
-        //         if (dataServicos.size() > 2) {
-        //             var folga = dataServicos.get(i + 1).compareTo(dataServicos.get(i)) - 1;
-        //             folgas.add(folga);
-        //         }
-        //     }
-
-        //     System.out.println("Folgas: " + folgas);
-        // }
+        militares.forEach(System.out::println);
 
     }
 
