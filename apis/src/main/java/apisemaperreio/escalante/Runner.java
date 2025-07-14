29003e0523e1cf0a52b5a4a6628921e9725cb668
João Anderson <joao.anderson.af@gmail.com>
2025-07-14 18:31:53 -0300

    Renomeando Instanciation para Runner e removendo código testado.

---
+++ apis/src/main/java/apisemaperreio/escalante/Runner.java
@@ -0,0 +1,15 @@
+package apisemaperreio.escalante;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Runner implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        
+    }
+
+}
