0a69d4f1c9d9f5d3a0d5a9e63b619d0168750cda
João Anderson <joao.anderson.af@gmail.com>
2025-07-22 20:58:26 -0300

    Criando classe para guardar estilos das planilhas exportadas.

---
+++ apis/src/main/java/apisemaperreio/escalante/escalante/utils/adapters/EstilosPlanilha.java
@@ -0,0 +1,61 @@
+package apisemaperreio.escalante.escalante.utils.adapters;
+
+import org.apache.poi.ss.usermodel.BorderStyle;
+import org.apache.poi.ss.usermodel.FillPatternType;
+import org.apache.poi.ss.usermodel.Font;
+import org.apache.poi.ss.usermodel.HorizontalAlignment;
+import org.apache.poi.ss.usermodel.IndexedColors;
+import org.apache.poi.ss.usermodel.VerticalAlignment;
+import org.apache.poi.xssf.usermodel.XSSFCellStyle;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class EstilosPlanilha {
+
+    protected XSSFCellStyle estiloPadrao;
+    protected XSSFCellStyle estiloCabecalho1;
+    protected XSSFCellStyle estiloCabecalho2;
+
+    public EstilosPlanilha(XSSFWorkbook workbook) {
+        this.estiloPadrao = definirEstiloPadrao(workbook);
+        this.estiloCabecalho1 = definirEstiloCabecalho1(workbook);
+        this.estiloCabecalho2 = definirEstiloCabecalho2(workbook);
+    }
+
+    private XSSFCellStyle definirEstiloCabecalho2(XSSFWorkbook workbook) {
+        var estilo = definirEstiloCabecalho1(workbook);
+        estilo.setFillForegroundColor(IndexedColors.GREY_25_PERCENT.getIndex());
+        return estilo;
+    }
+
+    private XSSFCellStyle definirEstiloCabecalho1(XSSFWorkbook workbook) {
+        var estilo = definirEstiloPadrao(workbook);
+        var fonte = definirFonte(workbook, true);
+        estilo.setFont(fonte);
+        estilo.setFillForegroundColor(IndexedColors.GREY_40_PERCENT.getIndex());
+        estilo.setFillPattern(FillPatternType.SOLID_FOREGROUND);
+        return estilo;
+    }
+
+    private XSSFCellStyle definirEstiloPadrao(XSSFWorkbook workbook) {
+        var estilo = workbook.createCellStyle();
+        var fonte = definirFonte(workbook, false);
+        estilo.setFont(fonte);
+        estilo.setAlignment(HorizontalAlignment.CENTER);
+        estilo.setVerticalAlignment(VerticalAlignment.CENTER);
+        estilo.setBorderTop(BorderStyle.THIN);
+        estilo.setBorderBottom(BorderStyle.THIN);
+        estilo.setBorderLeft(BorderStyle.THIN);
+        estilo.setBorderRight(BorderStyle.THIN);
+        return estilo;
+    }
+
+    private Font definirFonte(XSSFWorkbook workbook, boolean negrito) {
+        var fonte = workbook.createFont();
+        fonte.setBold(negrito);
+        fonte.setFontHeight(12);
+        fonte.setFontName("Arial");
+        fonte.setColor(IndexedColors.BLACK.getIndex());
+        return fonte;
+    }
+
+}
