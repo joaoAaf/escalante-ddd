31842b0e5b372a00ff92e9c0b9c5708438e51453
João Anderson <joao.anderson.af@gmail.com>
2025-10-16 19:07:34 -0300

    Adicionando componente militares ao `App.jsx`

--- front/escalante/src/App.jsx
+++ front/escalante/src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css'
+import Militares from './components/militares'
 import Sidebar from './components/sidebar'
 
 function App() {
   return (
     <div className="container">
       <Sidebar />
+      <Militares />
     </div>
   )
 }
