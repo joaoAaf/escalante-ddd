06915005d5961a86fcf73bdceebfbd86bf2d8c47
João Anderson <joao.anderson.af@gmail.com>
2025-10-16 20:39:40 -0300

    Interligando sidebar ao componente militares

--- front/escalante/src/App.jsx
+++ front/escalante/src/App.jsx
@@ -1,12 +1,20 @@
+import { useState } from 'react'
 import './App.css'
 import Militares from './components/militares'
 import Sidebar from './components/sidebar'
 
 function App() {
+  const [telaAtiva, setTelaAtiva] = useState('militares')
+
   return (
     <div className="container">
-      <Sidebar />
-      <Militares />
+      <Sidebar
+        telaAtiva={telaAtiva}
+        setTelaAtiva={setTelaAtiva}
+      />
+      <div className='main-content'>
+        {telaAtiva === 'militares' && <Militares />}
+      </div>
     </div>
   )
 }
