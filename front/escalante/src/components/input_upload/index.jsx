51aa2eb74a4ddcee0854429b590674c007224538
João Anderson <joao.anderson.af@gmail.com>
2025-10-19 21:27:55 -0300

    Implementando o componente de upload de arquivos e a função para fazer a requisição dos militares escaláveis

--- front/escalante/src/components/input_upload/index.jsx
+++ front/escalante/src/components/input_upload/index.jsx
@@ -1,23 +1,35 @@
+import { listarMilitaresEscalaveis } from '../../../client/listarMilitaresEscalaveis'
 import Styles from './styles.module.css'
 import { useState } from 'react'
 
 export default function InputUpload() {
     const [nomeArquivo, setNomeArquivo] = useState("Nenhum arquivo selecionado.")
+    const [arquivo, setArquivo] = useState(null)
 
-    const gerenciarNomeArquivo = (evento) => {
-        if (evento.target.files.length > 0)
+    const gerenciarArquivo = (evento) => {
+        if (evento.target.files.length > 0) {
             setNomeArquivo(evento.target.files[0].name)
+            setArquivo(evento.target.files[0])
+        }
+    }
+
+    const enviarArquivo = () => {
+        if (arquivo) {
+            const data = listarMilitaresEscalaveis(arquivo)
+            data.then(res => console.log(res))
+        } else
+            alert("Por favor, selecione um arquivo primeiro.")
     }
 
     return (
         <div className={Styles.input_upload}>
-            <input type="file" id="input_upload" onChange={gerenciarNomeArquivo} hidden />
+            <input type="file" id="input_upload" onChange={gerenciarArquivo} hidden />
 
             <label htmlFor="input_upload" className={Styles.browse_btn}>Importar</label>
 
             <span className={Styles.file_name}>{nomeArquivo}</span>
 
-            <button className={Styles.upload_btn}>Enviar/Processar</button>
+            <button className={Styles.upload_btn} onClick={enviarArquivo}>Enviar/Processar</button>
         </div>
     )
 }
