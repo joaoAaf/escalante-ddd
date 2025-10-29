75404072f318d3993bdda8387ad73d16a8c80967
João Anderson <joao.anderson.af@gmail.com>
2025-10-29 17:41:23 -0300

    Criando o componente CadastroServico.

--- front/escalante/src/components/escala/index.jsx
+++ front/escalante/src/components/escala/index.jsx
@@ -2,8 +2,12 @@ import BarraPesquisa from '../barra_pesquisa'
 import TabelaEscala from '../tabela_escala'
 import Styles from './styles.module.css'
 import { obterLocalStorage, salvarLocalStorage } from '../../scripts/persistenciaLocal'
+import { useState } from 'react'
+import CadastroServico from '../cadastro_servico'
 
 export default function Escala({ escala, setEscala }) {
+
+    const [statusModal, setStatusModal] = useState(false)
 
     const STORAGE_KEY_ESCALA = 'escala'
 
@@ -13,10 +17,17 @@ export default function Escala({ escala, setEscala }) {
     return (
         <div className={Styles.main}>
             <h2>Escala de Serviço</h2>
+            <div className={Styles.adicionarServico}>
+                <button onClick={() => setStatusModal(true)}>Adicionar Serviço</button>
+            </div>
             <BarraPesquisa />
             <TabelaEscala
                 escala={escala}
                 setEscala={setEscala}
+            />
+            <CadastroServico
+                statusModal={statusModal}
+                fecharModal={() => setStatusModal(false)}
             />
         </div>
     )
