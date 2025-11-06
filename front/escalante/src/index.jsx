02e35167ab86b9e3278a9e611271388126712b3a
João Anderson <joao.anderson.af@gmail.com>
2025-11-06 18:06:12 -0300

    Adicionando rotas para os componentes Militares e Escala, implementando navegação com react-router-dom e refatorando o componente App para suportar a nova estrutura de rotas.

--- front/escalante/src/index.jsx
+++ front/escalante/src/index.jsx
@@ -2,9 +2,23 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Militares from './routes/militares'
+import Escala from './routes/escala'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      { path: '/', element: <Militares /> },
+      { path: '/escala', element: <Escala /> },
+    ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
