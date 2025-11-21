319683d57e8529a71c309abc565db382734be4bd
João Anderson <joao.anderson.af@gmail.com>
2025-11-21 11:07:37 -0300

    Adicionando teste para importação de escala a partir de arquivo XLSX

--- apis/src/test/java/apisemaperreio/escalante/escalante/EscalanteTest.java
+++ apis/src/test/java/apisemaperreio/escalante/escalante/EscalanteTest.java
@@ -3,12 +3,16 @@ package apisemaperreio.escalante.escalante;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.time.LocalDate;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.web.multipart.MultipartFile;
 
+import apisemaperreio.escalante.escalante.domain.Funcao;
+import apisemaperreio.escalante.escalante.utils.adapters.importador_xlsx.ImportadorEscalaXLSXAdapter;
 import apisemaperreio.escalante.escalante.utils.adapters.importador_xlsx.ImportadorMilitaresXLSXAdapter;
 
 @SpringBootTest
@@ -17,9 +21,12 @@ class EscalanteTest {
 	@Autowired
 	private ImportadorMilitaresXLSXAdapter importadorMilitaresXLSXAdapter;
 
-	@Test
-	public void importarMilitaresXLSXTest_return25True7() throws Exception {
+	@Autowired
+	private ImportadorEscalaXLSXAdapter importadorEscalaXLSXAdapter;
 
+	@Test
+	public void deveImportarMilitaresXLSXQuandoPlanilhaPresenteEValida() throws Exception {
+		// Dados
 		final int QUANTIDADE_MILITARES_ESCALAVEIS = 25;
 		final String MATRICULA_MILITAR_FOLGA_ESPECIAL = "REG835";
 		final int FOLGA_ESPECIAL = 7;
@@ -31,17 +38,42 @@ class EscalanteTest {
 				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
 				inputStream);
 
+
+		// Quando
 		var militaresEscalaveis = importadorMilitaresXLSXAdapter.importarMilitaresXLSX(multipartFile);
-
-		assertEquals(QUANTIDADE_MILITARES_ESCALAVEIS, militaresEscalaveis.size());
-
 		var militarFolgaEspecial = militaresEscalaveis.stream()
 				.filter(militar -> militar.matricula().equals(MATRICULA_MILITAR_FOLGA_ESPECIAL)).findFirst();
 
+		// Então
+		assertEquals(QUANTIDADE_MILITARES_ESCALAVEIS, militaresEscalaveis.size());
 		assertTrue(militarFolgaEspecial.isPresent());
-
 		assertEquals(FOLGA_ESPECIAL, militarFolgaEspecial.get().folgaEspecial());
+	}
 
+	@Test
+	public void deveImportarEscalaXLSXQuandoPlanilhaPresenteEValida() throws Exception {
+		// Dados
+		final int QUANTIDADE_SERVICOS_OPERACIONAIS = 156;
+		final LocalDate DATA_19_09_2024 = LocalDate.of(2024, 9, 19);
+		final String MATRICULA_MILITAR_COV_19_09_2024 = "REG273";
+
+		var inputStream = getClass().getResourceAsStream("/samples/modelo_importacao_escala.xlsx");
+
+		MultipartFile multipartFile = new MockMultipartFile(
+				"file", "modelo_importacao_escala.xlsx",
+				"application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+				inputStream);
+
+		// Quando
+		var servicosOperacionais = importadorEscalaXLSXAdapter.importarEscalaXLSX(multipartFile);
+		var servicoCov19092024 = servicosOperacionais.stream()
+				.filter(servico -> servico.dataServico().isEqual(DATA_19_09_2024) && servico.funcao().equals(Funcao.COV.getNome()))
+				.findFirst();
+		
+		// Então
+		assertEquals(QUANTIDADE_SERVICOS_OPERACIONAIS, servicosOperacionais.size());
+		assertTrue(servicoCov19092024.isPresent());
+		assertEquals(MATRICULA_MILITAR_COV_19_09_2024, servicoCov19092024.get().matricula());
 	}
 
 }
